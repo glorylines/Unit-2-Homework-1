3a5f4fcdb147e3d8654e32af29d36a32229f394c
Ainura Sarsenbayeva <sarsenbaeva2000@mail.ru>
2023-06-27 02:19:22 +0600

    homework_1

---
+++ index.js
@@ -0,0 +1,19 @@
+alert("Заполните анкету");
+
+const fullName = prompt("Ваше ФИО:");
+console.log(fullName)
+
+const age = prompt("Ваш возраст в годах:");
+console.log(age)
+
+const ageInDays = prompt("Ваш возраст в днях:");
+console.log(ageInDays)
+
+const result = prompt("Через 5 лет вам будет:");
+console.log(result)
+
+const gender = confirm("Ваш пол - женский?");
+console.log(gender)
+
+const pension = confirm("Вы на пенсии:");
+console.log(pension)
